8c158d47ebceca2df7b751c275a3e7735bb7e641
Xiaomuchen0715 <teffechen@gmail.com>
2017-04-09 08:59:21 -0400

    combined clean function

--- Newfunction.R
+++ Newfunction.R
@@ -9,28 +9,18 @@ CleanFun<-function(frame,x){
   idx<-attr(matches,"match.length")
   idx<-idx<=2&idx>0
   x<-x[idx]#11688 offenses
+  framn<-frame[idx,]#11688 offenses
   matches<-gregexpr("[0-9]+",x)
   FullCode<-regmatches(x,matches)
   FullCode<-sapply(FullCode,"[[",1)
+  framn<-cbind(framn,FullCode)
   FullCode[which(nchar(FullCode) == 2)]<-"13(a)"
   FullCode[which(nchar(FullCode) == 4)]<-substr(FullCode[which(nchar(FullCode) == 4)],1,2)
   FullCode[which(nchar(FullCode) == 3)]<-substr( FullCode[which(nchar(FullCode) == 3)],1,1)
-  framn<-frame[idx,]
-  cbind(framn,FullCode)
+  ShortCode<-FullCode
+  framn<-cbind(framn,ShortCode)
   return(framn)
 }
 
-c<-CleanFun(datwhole,datwhole$OFFENSES)
-
-matches<-regexpr("[0~9]+",datwhole$OFFENSES)
-idx<-attr(matches,"match.length")
-idx<-idx<=2&idx>0
-datwhole<-datwhole[idx,]#11688 offenses
-matches<-gregexpr("[0-9]+",datwhole$OFFENSES)
-ShortCode<-regmatches(datwhole$OFFENSES,matches)
-datwhole$ShortCode<-sapply(ShortCode,"[[",1)
-datwhole$ShortCode[which(nchar(datwhole$ShortCode) == 2)]<-"13(a)"
-datwhole$ShortCode[which(nchar(datwhole$ShortCode) == 4)]<-substr( datwhole$ShortCode[which(nchar(datwhole$ShortCode) == 4)],1,2)
-datwhole$ShortCode[which(nchar( datwhole$ShortCode) == 3)]<-substr( datwhole$ShortCode[which(nchar(datwhole$ShortCode) == 3)],1,1)
-
+dat<-CleanFun(datwhole,datwhole$OFFENSES)
 sort(table(datwhole$ShortCode),decreasing = T)
