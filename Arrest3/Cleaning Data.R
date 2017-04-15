93809cb4d01c8a1b895fa567cb1083c6f3af3572
Jinshi Deng <Jinshi Deng>
2017-04-15 13:12:01 -0400

    New One

--- Arrest3/Cleaning Data.R
+++ Arrest3/Cleaning Data.R
@@ -1,114 +1,3 @@
-rm(list=ls())
-setwd("C:/Users/dengj/Desktop/2016-2017 Spring/R/data")
-dat=read.csv("ArrestData.csv")
-y<-regexpr("/",dat$OFFENSES)
-idx<-which(y>0)
-offense<-NULL
-i=0
-for(n in 1:length(idx)){
-  i=i+1
-  offense[i]<-substr(dat$OFFENSES[idx[n]],1,y[idx[n]]-1)
-}
-dat1<-dat[idx,]
-dat1$OFFENSES<-offense
-dat2<-dat[-idx,]
-arrest<-rbind(dat1,dat2)
-
-y6<-regexpr(" ",arrest$OFFENSES)
-code<-NULL
-i=0
-for(n in 1:length(y6)){
-  i=i+1
-  code[i]<-substr(arrest$OFFENSES[n],1,y6[n]-1)
-}
-shortcode<-code
-arrest<-data.frame(arrest$AGE,arrest$GENDER,arrest$ARRESTTIME,arrest$OFFENSES,arrest$RACE,code,shortcode)
-
-y1<-regexpr("[(]",arrest$code)
-idx1<-which(y1>0)
-code1<-NULL
-i=0
-for(n in 1:length(idx1)){
-  i=i+1
-  code1[i]<-substr(arrest$code[idx1[n]],1,y1[idx1[n]]-1)
-}
-arrest1<-arrest[idx1,]
-arrest1$code<-code1
-arrest2<-arrest[-idx1,]
-arrest<-rbind(arrest1,arrest2)
-
-y2<-regexpr("[.]",arrest$code)
-idx2<-which(y2>0)
-code2<-NULL
-i=0
-for(n in 1:length(idx2)){
-  i=i+1
-  code2[i]<-substr(arrest$code[idx2[n]],1,y2[idx2[n]]-1)
-}
-arrest1<-arrest[idx2,]
-arrest1$code<-code2
-arrest2<-arrest[-idx2,]
-arrest<-rbind(arrest1,arrest2)
-
-idx5<-which(arrest$code==13)
-arrest$code[idx5]<-"13(a)"
-arrest$shortcode<-arrest$code
-
-idx6<-which(nchar(arrest$shortcode)==3)
-idx7<-which(nchar(arrest$shortcode)==4)
-
-arrest$shortcode[idx6]<-substr(arrest$shortcode[idx6],1,1) 
-arrest$shortcode[idx7]<-substr(arrest$shortcode[idx7],1,2) 
-arrest<-na.omit(arrest)
-table(arrest$arrest.AGE)
-
-idx8<-which(arrest$arrest.AGE==0|arrest$arrest.AGE==999)
-arrest<-arrest[-idx8,]
-
-idx9<-which(arrest$shortcode=="27"|arrest$shortcode=="13(a)"|arrest$shortcode=="9"|arrest$shortcode=="39")
-ArrestTopFour<-arrest[idx9,]
-sort(table(ArrestTopFour$shortcode))
-
-library(lubridate)
-table(year(ArrestTopFour$arrest.ARRESTTIME))
-
-idx10<-which(year(ArrestTopFour$arrest.ARRESTTIME)==2014)
-ArrestTopFour<-ArrestTopFour[-idx10,]
-sort(table(ArrestTopFour$shortcode))
-
-ArrestTopFour<-data.frame(ArrestTopFour$arrest.AGE,ArrestTopFour$arrest.GENDER,ArrestTopFour$arrest.RACE,ArrestTopFour$arrest.OFFENSES,ArrestTopFour$code,ArrestTopFour$shortcode)
-
-colnames(ArrestTopFour)<-c("AGE","GENDER","RACE","OFFENSE","CODE","SHORTCODE")
-
-
-#ggplot2
-install.packages("ggplot2")
-library(ggplot2)
-library(plyr)
-
-ggplot(data=ArrestTopFour,aes(x=AGE,fill=GENDER)) + 
-  geom_bar(data=subset(ArrestTopFour,GENDER=="F")) + 
-  geom_bar(data=subset(ArrestTopFour,GENDER=="M"),aes(y=..count..*(-1))) + 
-  scale_y_continuous(breaks=seq(-100,100,10),labels=abs(seq(-100,100,10))) + 
-  coord_flip()+
-  facet_grid(SHORTCODE~.)
-
-
-
-datgraph<-dat
-idx3<-which(datgraph$AGE>0 & datgraph$AGE<100)
-datgraph<-datgraph[idx3,]
-
-ggplot(data=datgraph,aes(x=AGE,fill=RACE)) + 
-  geom_bar(data=subset(datgraph,RACE=="B")) + 
-  geom_bar(data=subset(datgraph,RACE=="W"),aes(y=..count..*(-1))) + 
-  scale_y_continuous(breaks=seq(-200,200,40),labels=abs(seq(-200,200,40))) + 
-  coord_flip()
-
-
-
-
-
 #Data Recleaning
 rm(list=ls())
 setwd("C:/Users/dengj/Desktop/2016-2017 Spring/R/data")
@@ -117,22 +6,27 @@ dat=read.csv("ArrestDataFinal.csv")
 #Data Recleaning
 
 library(lubridate)
-idx<-which(dat$AGE<=21&dat$AGE>0)
+idx<-which(dat$AGE<100&dat$AGE>0)
 dat1<-dat[idx,]
 idx1<-which(year(dat1$ARRESTTIME)==2014|year(dat1$ARRESTTIME)==2015)
 dat1<-dat1[-idx1,]
 ArrestTeen<-data.frame(dat1$X_id,dat1$AGE,dat1$GENDER,dat1$RACE,dat1$ARRESTTIME,dat1$OFFENSES,dat1$ARRESTLOCATION,dat1$PK,dat1$CCR)
+ArrestTeen$TENN<-ArrestTeen$dat1.AGE<22
+ArrestTeen$TENN[which(ArrestTeen$TENN=="TRUE")]<-1
+ArrestTeen$TENN[which(ArrestTeen$TENN=="FALSE")]<-0
+
 library(lubridate)
 arresttime<-ArrestTeen$dat1.ARRESTTIME
 arresttime2<-sub("T", " ", arresttime)
-arresttime3 <- as.POSIXct(arresttime2)
+arresttime3<-as.POSIXlt(arresttime2,format = "%Y-%m-%d %H:%M:%S")
 hour2<-as.character(hour(arresttime3))
 months2<-as.character(months(arresttime3))
 weekdays2<-as.character(weekdays(arresttime3))
 ArrestTeen$dat1.ARRESTLOCATION<-hour2
 ArrestTeen$dat1.PK<-months2
 ArrestTeen$dat1.CCR<-weekdays2
-colnames(ArrestTeen)<-c("ID","AGE","GENDER","RACE","ArrestTime","OFFENSE","Hour","Month","Weekday")
+colnames(ArrestTeen)<-c("ID","AGE","GENDER","RACE","ArrestTime","OFFENSE","Hour","Month","Weekday","Teen")
+ArrestTeenForVincent<-ArrestTeen
 
 library(dplyr)
 library(tidyr)
@@ -207,30 +101,4 @@ ArrestTeen$ShortCode[idx8]<-substr(ArrestTeen$ShortCode[idx8],1,2)
 
 
 #Get Unique ShortCode for EACH One Offender
-ArrestTeen1<-distinct(ArrestTeen,ID,AGE,GENDER,RACE,ArrestTime,Hour,Month,Weekday,ShortCode)
-
-#write CSV
-setwd("C:/Users/dengj/Desktop/2016-2017 Spring/R/data")
-write.csv(ArrestTeen,"C:/Users/dengj/Desktop/2016-2017 Spring/R/data/ArrestTeen.csv")
-write.csv(ArrestTeen1,"C:/Users/dengj/Desktop/2016-2017 Spring/R/data/ArrestTeen1.csv")
-
-
-
-#Data Cleaning Rewite
-rm(list=ls())
-setwd("C:/Users/dengj/Desktop/2016-2017 Spring/R/data")
-dat=read.csv("ArrestDataFinal.csv")
-
-#as.date  Extract only the crimes happening in 2016&2017
-library(lubridate)
-idx<-which(year(dat$ARRESTTIME)==2014|year(dat$ARRESTTIME)==2015)
-dat<-dat[-idx,]
-
-#Remove Outliers
-idx1<-which(dat$AGE>0 & dat$AGE<100)
-dat<-dat[idx1,]
-
-#Creat Binary Variable
-Arrest<-data.frame(dat$X_id,dat$AGE,dat$GENDER,dat$RACE,dat$ARRESTTIME,dat$OFFENSES)
-Arrest$TENN<-Arrest$dat.AGE<22
-table(Arrest$TENN)
+ArrestTeenForYilin<-distinct(ArrestTeen,ID,AGE,GENDER,RACE,ArrestTime,Hour,Month,Weekday,ShortCode)
