6d19075cc351009bbb311acf1aeeb51052238f0b
Yilin <suso0417@gmail.com>
2017-04-17 22:14:50 -0400

    Final version of Main-Team 3

--- Arrest3/Main-Team 3.R
+++ Arrest3/Main-Team 3.R
@@ -126,6 +126,7 @@ idx13<-which(ArrestTeen$ShortCode=="13(a)")
 Age13<-ArrestTeen[idx13,]
 AgeTopCrime<-rbind(Age27,Age13)
 
+
 ##Divide age by age group: elementary school, junior high school, senior high school
 agegroup<-ifelse(AgeTopCrime$AGE<=13,"10-13",ifelse(AgeTopCrime$AGE<=18,"14-18","19-21"))
 AgeTopCrime<-cbind(AgeTopCrime,agegroup)
@@ -151,6 +152,76 @@ c+geom_bar(stat="count")+
   facet_grid(.~AgeTopCrime$ShortCode)+
   ggtitle("Top 2 Crimes Among Teenagers")
 
+#Revised Edition in week 14:
+ArrestTeen=read.csv("ArrestTeenForYilin.csv")
+
+###test
+
+str(ArrestTeen)
+summary(ArrestTeen)
+ArrestTeen[1:6,]
+####For Teen:
+Teen<-which(ArrestTeen$Teen==1)
+Teendt<-ArrestTeen[Teen,]
+count <- table(Teendt$ShortCode)
+subdat <- data.frame(shortCode=names(count),value=as.vector(count))
+subdat[1:6,]
+subdatTeen<-subdat[which(subdat$value>0),]
+
+###For Adult:
+Adult<-which(ArrestTeen$Teen==0)
+Adultdt<-ArrestTeen[Adult,]
+count2 <- table(Adultdt$ShortCode)
+subdat2 <- data.frame(shortCode=names(count2),value=as.vector(count2))
+subdat2[1:6,]
+subdatAdult<-subdat2[which(subdat2$value>0),]
+
+###Graph for total distribution of crimes among teens(1):
+library(ggplot2)
+Teen<-ggplot(data=subdatTeen, aes(x=reorder(shortCode, value), y=value)) 
+Teen+geom_bar(stat="identity",fill='blue')+
+  theme(axis.text.x = element_text(angle = 45, hjust = 1))+
+  xlab("Short Offense Code")+
+  scale_fill_manual(values=Blue)+
+  geom_text(aes(label=value), check_overlap = TRUE,vjust=2, colour="black")+
+  ggtitle("Distribution of All Crime Types Among Teenagers")+
+  coord_flip()
+
+
+###Graph for total distribution of crimes among adult(2):
+Adult<-ggplot(data=subdatAdult, aes(x=reorder(shortCode, value), y=value)) 
+Adult+geom_bar(stat="identity",fill='steelblue')+
+  theme(axis.text.x = element_text(angle = 45, hjust = 1))+
+  xlab("Short Offense Code")+
+  scale_fill_manual(values=Blue)+
+  geom_text(aes(label=value), check_overlap = TRUE,vjust=2, colour="black")+
+  ggtitle("Distribution of All Crime Types Among Adults")+
+  coord_flip()
+
+####Graph Comparison For Top 4 using percentage (3):
+idx<-which(ArrestTeen$ShortCode==27|ArrestTeen$ShortCode==9|ArrestTeen$ShortCode=="13(a)"|ArrestTeen$ShortCode==39)
+TopFour<-ArrestTeen[idx,]
+TopFour$Teen[which(TopFour$Teen=="1")]<-"Teen"
+TopFour$Teen[which(TopFour$Teen=="0")]<-"Adult"
+table(TopFour$Teen)
+
+library(scales)
+Four<-ggplot(TopFour,aes(x=ShortCode,fill=Teen))
+Four+geom_bar(aes(y = (..count..)/sum(..count..)),position="dodge") + 
+  scale_fill_manual(values = c("darkblue", "#6698FF"))+
+  ggtitle("Top 4 Crimes--Adult vs. Teen")+
+  ylab("Percentage %")+
+  geom_text(aes(y = ((..count..)/sum(..count..)), label = scales::percent((..count..)/sum(..count..))), stat = "count", position = position_dodge(0.9),vjust =-0.5)
+
+###Another solution Top 4 (4):
+Percentage <- read_csv("~/Desktop/Percentage.csv")
+c<-ggplot(data=Percentage,aes(x=ShortCode,y=`Percent(%)`,fill=Teen))
+c+geom_bar(stat="identity",position="dodge")+
+  scale_fill_manual(values = c("darkblue", "#6698FF"))+
+  labs(title = "Top 4 Crimes--Adult vs. Teen",
+       caption = "Graph by Yilin Lu")+
+  geom_text(aes(label=`Percent(%)`), check_overlap = TRUE,position = position_dodge(0.9),vjust=-1, colour="black")
+###
 
 # Graph 2.3 & 2.4: Vincent Fan
 #library useful packages
